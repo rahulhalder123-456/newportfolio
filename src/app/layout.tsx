e1a462aad4137bb31c9161e35d63fe0266b444e2
rahulhalder123-456 <rahulhalder123987@gmail.com>
2025-06-26 04:41:39 +0530

    Update favicon link in RootLayout component for correct path resolution

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark scroll-smooth no-scrollbar">
       <head>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="any" />
+        <link rel="icon" href="@/app/favicon.ico" type="image/x-icon" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
